753e4029f3088cbe752b92f7b5ba8b04a0915de8
kedardutt <37819166+kedardutt@users.noreply.github.com>
2018-12-10 07:14:35 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).foundation();
+
+$(document).ready(function(){
+
+
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+      tabcontent[0].style.display = "block";
+});
+
+
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
